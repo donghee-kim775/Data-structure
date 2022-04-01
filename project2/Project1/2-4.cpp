968e7051820dbfefcf95a66d7fec35df146ffb9e
DongHee Kim <54052704+donghee-kim775@users.noreply.github.com>
2022-04-01 16:26:49 +0900

    example 2

---
+++ project2/Project1/2-4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() {
+	char str[20] = "Data Structrue!";
+	int i;
+	printf("\n문자배열 str : ");
+	for ( i = 0; i < str[i]; i++){
+		printf("%c", str[i]);
+	}
+	getchar();
+}
