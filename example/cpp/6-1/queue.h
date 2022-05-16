9724bdebe06fb275050dc9b2d6e5fb75894df0d0
DongHee Kim <54052704+donghee-kim775@users.noreply.github.com>
2022-05-16 18:03:08 +0900

    update

---
+++ example/cpp/6-1/queue.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+#define Q_SIZE 4
+
+typedef char element;      // 큐 원소(element)의 자료형을 char로 정의
+typedef struct {
+	element queue[Q_SIZE];   // 1차원 배열 큐 선언
+	int front, rear;
+} QueueType;
+
+QueueType* createQueue();
+
+int isEmpty(QueueType* Q);
+
+int isFull(QueueType* Q);
+
+void enQueue(QueueType* Q, element item);
+
+element deQueue(QueueType* Q);
+
+element peek(QueueType* Q);
+
+void printQ(QueueType* Q);
+#endif // !__QUEUE_H__
