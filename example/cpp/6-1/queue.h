67f2be5235647223f424e72d658b6c42f26480a1
DongHee Kim <54052704+donghee-kim775@users.noreply.github.com>
2022-05-16 18:37:09 +0900

    update

--- example/cpp/6-1/queue.h
+++ example/cpp/6-1/queue.h
@@ -3,9 +3,9 @@
 #define __QUEUE_H__
 #define Q_SIZE 4
 
-typedef char element;      // 큐 원소(element)의 자료형을 char로 정의
+typedef char element;
 typedef struct {
-	element queue[Q_SIZE];   // 1차원 배열 큐 선언
+	element queue[Q_SIZE];
 	int front, rear;
 } QueueType;
 
