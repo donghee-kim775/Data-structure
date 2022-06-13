97035f84807815ffe64725b927f8ebbe626f77c3
DongHee Kim <54052704+donghee-kim775@users.noreply.github.com>
2022-06-13 18:00:31 +0900

    update

---
+++ example/cpp/8-1/adjMtrix.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef __AdjMatrix_H__
+#define __AdjMatrix_H__
+
+#define MAX_VERTEX 30
+
+typedef struct graphType {
+	int n;
+	int adjMatrix[MAX_VERTEX][MAX_VERTEX];
+} graphType;
+
+void createGraph(graphType* g);
+void insertVertex(graphType* g, int v);
+void insertEdge(graphType* g, int u, int v);
+void print_adjMatrix(graphType* g);
+
+#endif
